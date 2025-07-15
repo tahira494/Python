a9feace539940090720418cc2cae98ef97b0d279
tahira494 <tahmina7413@gmail.com>
2025-07-15 19:42:21 +0600

    Add files via upload

---
+++ 104_intro.py
@@ -0,0 +1,14 @@
+a = 10
+print(type(a))
+b = 3.14
+print(type(b))
+c = "Hello"
+print(type(c))
+d = True
+print(type(d))
+e = [1,2,3]
+print(type(e))
+f = (4,5,6)
+print(type(f))
+g = {'x': 1,'y':2}
+print(type(g))
