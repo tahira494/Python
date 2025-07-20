1b8992635a058b63206d33f5c20cbc4cbaec8ae6
tahira494 <tahmina7413@gmail.com>
2025-07-20 19:42:44 +0600

    Add files via upload

---
+++ 105_intro.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+c = int(input("Enter thethird number:"))
+d = int(input("Enter the fourth number:"))
+e = int(input("Enter the fifth number:"))
+
+total= a + b + c + d + e
+avg= total/5
+
+
+print("Result:",avg)
