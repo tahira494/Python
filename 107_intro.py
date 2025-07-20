3190afc1132dd4af365e95054fe3847e4683867e
tahira494 <tahmina7413@gmail.com>
2025-07-20 21:35:44 +0600

    Add files via upload

---
+++ 107_intro.py
@@ -0,0 +1,19 @@
+a = 20
+print(type(a))
+
+b = 15.3
+print(type(b))
+
+c ="Hello world"
+print(type(c))
+
+d = False
+print(type(d))
+
+e = [4,5,6]
+print(type(e))
+
+f = (7,8,9)
+print(type(f))
+
+g = {'h': 1, 'i': 2}
