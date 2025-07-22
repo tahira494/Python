b0068eceac82c0d39b6b5f06ab829a96d8bf3635
tahira494 <tahmina7413@gmail.com>
2025-07-22 19:45:00 +0600

    Add files via upload

---
+++ 109_avg.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+c = int(input("Enter the third number:")) 
+
+total = a + b + c
+avg = total/3
+print("Result:" , avg)
---
+++ 111_profit loss.py
@@ -0,0 +1,8 @@
+buy = int(input("Enter the buying price:")) 
+sell = int(input("Enterthe selling price:"))
+ 
+if sell > buy:
+    print("You have made profit.")
+
+else:
+    print("You made loss.")
