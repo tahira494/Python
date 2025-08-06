805c3e4b9129a8bdd897ee17c66daef014fb5af9
tahira494 <tahmina7413@gmail.com>
2025-08-06 19:44:51 +0600

    Add files via upload

---
+++ 120_nasted else if.py
@@ -0,0 +1,12 @@
+a = int(input("Enter a number:"))
+
+
+if a>0 :
+  if a==0 :
+    print("The number is zero.")
+
+  else :
+    print("The number is positive.")
+
+else :
+  print("The number is negative.")
