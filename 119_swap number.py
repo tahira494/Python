ef5a2a33045c889f88e422f192300425bd6436ce
tahira494 <tahmina7413@gmail.com>
2025-08-10 17:45:51 +0600

    Add files via upload

---
+++ 119_swap number.py
@@ -0,0 +1,21 @@
+print("==Number Swaping==")
+
+
+a = int(input("Enter the first number(a):"))
+b = int(input("Enter the second number(b):"))
+
+
+print("/nbefore swaping:")
+print("a=", a)
+print("b=", b)
+
+
+
+temp = a 
+a = b 
+b = temp
+
+
+print("/nafter swaping")
+print("a =", a)
+print("b = ",b)
